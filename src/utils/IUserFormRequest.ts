8a5c9a41dbaf9e22f6bee4ad85667103e58fba67
Ankush Thakur <atpara99@gmail.com>
2024-05-01 11:11:33 +0530

    formik implemented

---
+++ src/utils/IUserFormRequest.ts
@@ -0,0 +1,11 @@
+export interface IuserFormRequest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  dob: string;
+  street1: string;
+  street2: string;
+  sameAsResidential: boolean;
+  permanentStreet1: string;
+  permanentStreet2: string;
+}
